ae2cf8d1c061feaedfd01e57bb5c8985e8ca4d4d
Gabriel Santini Quibao <49206000+gquibao@users.noreply.github.com>
2019-08-02 22:58:15 -0300

    Commit 2
    Script de controle do player e colisão da arma inseridos.

---
+++ Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    public static PlayerActions instance;
+
+    bool attack = false;
+    bool defense = false;
+    public bool hit = false;
+    public Animator anim;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void OnClickEnter()
+    {
+        StartCoroutine(timer());
+    }
+
+    public void OnClickExit()
+    {
+        StopAllCoroutines();
+        if(defense)
+        {
+            defense = false;
+            anim.SetBool("Defend", defense);
+        }
+        else if(!defense && !attack)
+        {
+            attack = true;
+            anim.SetTrigger("Melee Right Attack 01");
+        }
+        attack = false;
+    }
+
+    IEnumerator timer()
+    {
+        yield return new WaitForSeconds(0.2f);
+        defense = true;
+        anim.SetBool("Defend", defense);
+    }
+
+}
---
+++ Assets/Scripts/WeaponCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponCollision : MonoBehaviour
+{
+    public bool isPlayer = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isPlayer)
+        {
+            if (other.tag == "Enemy")
+            {
+                Debug.Log("Player Score");
+                PlayerActions.instance.hit = true;
+            }
+        }
+
+        else if (!isPlayer)
+        {
+            if (other.tag == "Player")
+            {
+                Debug.Log("Enemy Score");
+            }
+        }
+    }
+}
